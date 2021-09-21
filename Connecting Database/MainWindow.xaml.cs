3b33cd1249be010d8d30c294d56201d32ec2c3d9
Tshiamo Monageng <90952386+snw-hub@users.noreply.github.com>
2021-09-21 15:27:09 +0200

    Add files via upload

---
+++ Connecting Database/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace connectingDatabase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection;
+        SqlDataReader dataReader;
+        SqlCommand command;
+        string sql,output="";
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+             connection = new SqlConnection(@"Data Source=RCPSCR9PC026\SQLEXPRESS;Initial Catalog=MySchool;Integrated Security=True");
+            connection.Open();
+
+            sql = "Select * from STUDENTS ";
+
+            command = new SqlCommand(sql, connection);
+
+            dataReader = command.ExecuteReader();
+
+            //while (dataReader.Read())
+            //{
+            //    output = output + dataReader.GetValue(0) + "\t" + dataReader.GetValue(2) + "\n";
+
+            //}
+            //  dataReader = command.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dataReader);
+
+            datagridDisplay.DataContext = dataTable;
+
+           // MessageBox.Show(output);
+
+            output = "";
+            dataReader.Close();
+            command.Dispose();
+            connection.Close();
+
+        }
+    }
+}
